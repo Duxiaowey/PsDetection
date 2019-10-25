c7d870cabd4197be6acbe945cc874389f3f9312d
duxiaowey <519780052@qq.com>
2019-10-25 15:04:28 +0800

    add comments to noise_stream_SRM_layer.py and use it to produce outputs

--- lib/layer_utils/noise_stream_SRM_layer.py
+++ lib/layer_utils/noise_stream_SRM_layer.py
@@ -22,6 +22,7 @@ def PlotImage(image):
     return (im - np.min(im)) / (np.max(im) - np.min(im))
 
 def SRM(imgs):
+    # 第一层滤波器
     # 定义三个滤波器,滤波器大小为5x5
     # filter1: egde3*3
     filter1 = [[0, 0, 0, 0, 0],
@@ -48,24 +49,29 @@ def SRM(imgs):
     filter3 = np.asarray(filter3, dtype=float) / 2
     # 将不同类的滤波器堆叠、处理，得到新滤波器
     filters = [[filter1, filter1, filter1], [filter2, filter2, filter2], [filter3, filter3, filter3]]
-    # new_filter(i,j,l,k) = origin_filter(k,l,i,j)
-    filters = np.einsum('klij->ijlk', filters)
+    filters = np.einsum('klij->ijlk', filters)  # new_filter(i,j,l,k) = origin_filter(k,l,i,j)
     filters = tf.Variable(filters, dtype=tf.float32)
     imgs = np.array(imgs, dtype=float)
     input = tf.Variable(imgs, dtype=tf.float32)
+    # 未标出的卷积参数：use_cudnn_on_gpu=True, data_format="NHWC", dilations=[1, 1, 1, 1], name=None
     op = tf.nn.conv2d(input, filters, strides=[1, 1, 1, 1], padding='SAME')
+    # 得到第一层输出：op
 
+    # 定义第二层滤波器，滤波方式同第一层
     q = [4.0, 12.0, 2.0]
+    # filter1: egde3*3
     filter1 = [[0, 0, 0, 0, 0],
                [0, -1, 2, -1, 0],
                [0, 2, -4, 2, 0],
                [0, -1, 2, -1, 0],
                [0, 0, 0, 0, 0]]
+    # filter2：egde5*5
     filter2 = [[-1, 2, -2, 2, -1],
                [2, -6, 8, -6, 2],
                [-2, 8, -12, 8, -2],
                [2, -6, 8, -6, 2],
                [-1, 2, -2, 2, -1]]
+    # filter3：一阶线性
     filter3 = [[0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0],
                [0, 1, -2, 1, 0],
@@ -76,7 +82,7 @@ def SRM(imgs):
     filter3 = np.asarray(filter3, dtype=float) / q[2]
     filters = [[filter1, filter1, filter1], [filter2, filter2, filter2], [filter3, filter3, filter3]]
     filters = np.einsum('klij->ijlk', filters)
-    filters = filters.flatten()
+    filters = filters.flatten()     # 将filters拉成一维
     initializer_srm = tf.constant_initializer(filters)
 
     # 分段函数:     x < -2, y = -2;     -2 < x < 2, y = x;     x > 2, y = 2
@@ -89,6 +95,10 @@ def SRM(imgs):
     op2 = slim.conv2d(input, 3, [5, 5], trainable=False, weights_initializer=initializer_srm,
                       activation_fn=None, padding='SAME', stride=1, scope='srm')
     op2 = truncate_2(op2)
+    # 将op2用(-2, 2)的分段函数激活
+    # 得到第二层输出：op2
+
+    # 定义第三层滤波器
     filter_coocurr = [[0, 0, 0, 0, 0, 0, 0],
                       [0, 0, 0, 0, 0, 0, 0],
                       [0, 0, 0, 0, 0, 0, 0],
@@ -103,12 +113,17 @@ def SRM(imgs):
                            [0, 0, 0, 0, 0, 0, 0],
                            [0, 0, 0, 0, 0, 0, 0],
                            [0, 0, 0, 0, 0, 0, 0]]
+    # filters_coocurr: 3 x 3 x 7 x 7
     filters_coocurr = [[filter_coocurr, filter_coocurr_zero, filter_coocurr_zero],
                        [filter_coocurr_zero, filter_coocurr, filter_coocurr_zero],
                        [filter_coocurr_zero, filter_coocurr_zero, filter_coocurr]]
     filters_coocurr = np.einsum('klij->ijlk', filters_coocurr)
+    # filters_coocurr: 7 x 7x 3 x 3
+    # 形似filter_coocurr的分块矩阵
+
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
+        # op：第一层的卷积输出；op2：第二层的卷积输出
         re = (sess.run(op))
         res = np.round(re[0])
         res[res > 2] = 2
@@ -116,8 +131,8 @@ def SRM(imgs):
 
         res2 = sess.run(op2)
         # print(sum(sum(sum(sum(res2>2)))))
-    ress2 = np.array(res2, dtype=float)
     ress = np.array([res], dtype=float)
+    ress2 = np.array(res2, dtype=float)
     # input = tf.Variable(ress, dtype=tf.float32)
     # op = tf.nn.conv2d(input, filters_coocurr, strides=[1, 1, 1, 1], padding='SAME')
     # with tf.Session() as sess:
@@ -130,6 +145,7 @@ if __name__ == '__main__':
     img = Image.open('lib/layer_utils/000009.jpg')
     img = np.asarray(img)
     img, img2 = SRM([img])
+    # img，img2为第一层、第二层滤波器提取后的特征图
     # img = np.sqrt(img)
     # print(img[0])
     # img = Image.fromarray(np.uint8(img[0]))
@@ -137,11 +153,13 @@ if __name__ == '__main__':
     # img[0, :, :, 0] = PlotImage(img[0, :, :, 0])
     # img[0, :, :, 1] = PlotImage(img[0, :, :, 1])
     # img[0, :, :, 2] = PlotImage(img[0, :, :, 2])
+
+    # img.shape = (1, 375, 500, 3)
     plt.imshow(img2[0])
-    plt.savefig('noise0.jpg')
+    plt.savefig('img_op2.jpg')
 
     plt.imshow(PlotImage(img[0]))
-    plt.savefig('noise1.jpg')
+    plt.savefig('img_op.jpg')
 
     # plt.imshow(img[0])
     # plt.show()
